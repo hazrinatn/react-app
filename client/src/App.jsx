f1d0fd22aac28f2903495327bd39574f72607d6b
hazrinatn <hazrinatn@gmail.com>
2022-03-26 21:46:19 +0700

    add api

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/products/" element={<ProductList />} />
+      <Route path="/product/:id" element={<Product />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
+};
+
---
+++ client/src/data.js
@@ -0,0 +1,71 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: 'https://images.unsplash.com/photo-1559056408-94245a95e30e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+        title: "SPRING SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "c8d5b9",
+    },
+    {
+        id: 2,
+        img: 'https://images.unsplash.com/photo-1559056552-d73fd92ba522?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "68B0AB",
+    },
+    {
+        id: 3,
+        img: 'https://images.unsplash.com/photo-1559056427-6fe5ce77f06d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+        title: "WINTER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "8FC0A9",
+    },
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        title: "FOR WOMEM",
+        cat: "female"
+    },
+    {
+        id: 2,
+        img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        title: "T-SHIRT",
+        cat: "t-shirt"
+    },
+    {
+        id: 3,
+        img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        title: "LIGHT JACKETS",
+        cat: "jacket"
+    },
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+        id: 2,
+        img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+        id: 3,
+        img: "https://www.gmm-tv.com/shop/image/cache/catalog/product/8-shirt/34-bad-buddy-series-t-shirt-th/1-540x540.jpg",
+    },
+    {
+        id: 4,
+        img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+        id: 5,
+        img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+        id: 6,
+        img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+]
